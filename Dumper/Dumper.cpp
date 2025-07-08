3bac18fb5ca1010e4e09b2e798c10e0841ac6cab
maxpsger <maximilian@schmidt-altdorf.de>
2025-07-08 20:12:24 +0200

    Update Dumper.cpp

--- Dumper/Dumper.cpp
+++ Dumper/Dumper.cpp
@@ -5,19 +5,19 @@
 
 #pragma comment(lib, "Dbghelp.lib")
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cout << "Usage: dumper.exe <PID>" << std::endl;
-        return 1;
-    }
+int main() {
+    DWORD pid;
+    std::cout << "Gib die PID des Prozesses ein, den du dumpen willst: ";
+    std::cin >> pid;
 
-    DWORD pid = atoi(argv[1]);
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
     if (hProcess == NULL) {
         std::cout << "Failed to open process: " << GetLastError() << std::endl;
+        system("pause");
         return 1;
     }
+
     TCHAR dumpFile[MAX_PATH];
     _stprintf_s(dumpFile, _T("dump_%u.dmp"), pid);
 
@@ -26,10 +26,11 @@ int main(int argc, char* argv[]) {
     if (hFile == INVALID_HANDLE_VALUE) {
         std::cout << "Failed to create dump file: " << GetLastError() << std::endl;
         CloseHandle(hProcess);
+        system("pause");
         return 1;
     }
 
-    BOOL sucess = MiniDumpWriteDump(
+    BOOL success = MiniDumpWriteDump(
         hProcess,
         pid,
         hFile,
@@ -39,20 +40,19 @@ int main(int argc, char* argv[]) {
         NULL
     );
 
-    if (sucess) {
-        std::cout << "Dump sucessful: " << dumpFile << std::endl;
-    }
-    else {
-        std::cout << "Dump failed" << GetLastError() << std::endl;
+    if (success) {
+        std::cout << "Dump erfolgreich: " << dumpFile << std::endl;
+    } else {
+        std::cout << "Dump fehlgeschlagen: " << GetLastError() << std::endl;
     }
 
     CloseHandle(hFile);
 
-    //Ends Process immidiatly
+    // Zielprozess beenden
     TerminateProcess(hProcess, 0);
     CloseHandle(hProcess);
 
-    //ends the dumper itself
+    // Sich selbst beenden
     ExitProcess(0);
 
     return 0;
